794a43c9d6614a92f42408293874fd13d4c7b719
Harder258 <jorge.erc007@gmail.com>
2023-08-25 08:56:50 +0000

    GrassKernel: Add permissive config

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ set -e
 [ ! -e "scripts/packaging/pack.sh" ] && git submodule init && git submodule update
 [ ! -d "toolchain" ] && echo "Make toolchain avaliable at $(pwd)/toolchain" && exit
 
-export KBUILD_BUILD_USER=Royna
+export KBUILD_BUILD_USER=DragonFox
 export KBUILD_BUILD_HOST=GrassLand
 
 PATH=$PWD/toolchain/bin:$PATH
@@ -19,6 +19,10 @@ if [ "$2" = "ksu" ]; then
 CONFIG_KSU=vendor/ksu.config
 fi
 
+if [ "$3" = "permissive" ]; then
+CONFIG_SELINUX=vendor/permissive.config
+fi
+
 if [ -z "$DEVICE" ]; then
 export DEVICE=a51
 fi
@@ -26,6 +30,6 @@ fi
 rm -rf out
 make O=out CROSS_COMPILE=aarch64-linux-gnu- CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar \
 	OBJDUMP=llvm-objdump READELF=llvm-readelf -j$(nproc) \
-	vendor/${DEVICE}_defconfig vendor/grass.config vendor/${DEVICE}.config $CONFIG_AOSP $CONFIG_KSU
+	vendor/${DEVICE}_defconfig vendor/grass.config vendor/${DEVICE}.config $CONFIG_AOSP $CONFIG_KSU ${CONFIG_SELINUX}
 make O=out CROSS_COMPILE=aarch64-linux-gnu- CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar \
 	OBJDUMP=llvm-objdump READELF=llvm-readelf ${FLAGS} -j$(nproc)
