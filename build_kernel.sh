f26dbbdb874e73273aadcd3c8b14df2b899c2767
ghazzor <kartikhis8o@gmail.com>
2024-04-18 20:51:51 +0530

    Remove packaging submodule and intigrate directly in tree
    Signed-off-by: ghazzor <kartikhis8o@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 set -e
 
-[ ! -e "scripts/packaging/pack.sh" ] && git submodule init && git submodule update
-[ ! -d "toolchain" ] && echo "Make toolchain avaliable at $(pwd)/toolchain" && exit
+[ ! -e "KernelSU/kernel/setup.sh" ] && git submodule init && git submodule update
+[ ! -d "toolchain" ] && echo "Toolchain not avaliable at $(pwd)/toolchain... installing toolchain" && bash setup.sh
 
 export KBUILD_BUILD_USER=Royna
 export KBUILD_BUILD_HOST=GrassLand
@@ -10,11 +10,15 @@ export KBUILD_BUILD_HOST=GrassLand
 PATH=$PWD/toolchain/bin:$PATH
 export LLVM_DIR=$PWD/toolchain/bin
 export kerneldir=$PWD
+export ANYKERNEL=$PWD/tools/build/akv3
+export TIME="$(date "+%Y%m%d")"
 
 if [ "$1" = "oneui" ]; then
 FLAGS=ONEUI=1
+SUFFIX=ONEUI
 else
 CONFIG_AOSP=vendor/aosp.config
+SUFFIX=AOSP
 fi
 
 if [ -z "$DEVICE" ]; then
@@ -57,5 +61,46 @@ LLVM_DIS='${LLVM_DIR}/llvm-dis'
 LLVM_NM='${LLVM_DIR}/llvm-nm'
 '
 
+make distclean
+mkdir -p kernel_zip
 make O=out $COMMON_FLAGS vendor/${DEVICE}_defconfig vendor/grass.config vendor/${DEVICE}.config $CONFIG_AOSP $CONFIG_KSU $CONFIG_SELINUX
 make O=out $COMMON_FLAGS ${FLAGS} -j$(nproc)
+
+echo "  Cleaning Stuff"
+rm -rf ${ANYKERNEL}/Image
+rm -rf ${ANYKERNEL}/config
+rm -rf ${ANYKERNEL}/dtb
+echo "  done"
+echo ""
+echo "  Copying Stuff"
+
+locations=(
+  "${kerneldir}/arch/arm64/boot"
+  "${kerneldir}/out/arch/arm64/boot"
+)
+
+found=false
+for location in "${locations[@]}"; do
+  if test -f "$location/Image"; then
+    found=true
+    break
+  fi
+done
+
+if ! $found; then
+  echo "  ERROR : image binary not found in any of the specified locations , fix compile!"
+  exit 1
+fi
+
+cp -r out/arch/arm64/boot/Image ${ANYKERNEL}/Image
+cp -r out/.config ${ANYKERNEL}/config
+cp -r out/arch/arm64/boot/dtb_exynos.img ${ANYKERNEL}/dtb
+echo "  done"
+echo ""
+echo "  Zipping Stuff"
+cd ${ANYKERNEL}
+rm -rf Grass*.zip
+zip -r9 Grass-${SUFFIX}-$DEVICE_${KSUSTAT}_${SESTAT}_${TIME}.zip * -x .git README.md *placeholder
+cd ${kerneldir}
+cp ${ANYKERNEL}/Grass*.zip kernel_zip/
+echo "  Kernel zip in ${kerneldir}/kernel_zip"
